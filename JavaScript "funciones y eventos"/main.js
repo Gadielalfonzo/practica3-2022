03102026134f25a05251e8f9374dbb1f58b54ac2
Gadiel alfonzo <gadiel.instituto@gmail.com>
2022-05-17 11:55:59 -0300

    trabajo de autos terminado

--- JavaScript "funciones y eventos"/main.js
+++ JavaScript "funciones y eventos"/main.js
@@ -3,14 +3,14 @@ function seleccion(){
     let resultado=null
     if ( operacion == 0 ){
         //Si el vehículo a la venta es un Ford Fiesta, el descuento es de un 5%
-        resultado=2900-(2900*0.05)
+        resultado=3900000-(3900000*0.05)
     }else{
         if (operacion == 1){
             //Si el vehículo a la venta es un Ford focus, el descuento es del 10%
-            resultado=3350-(3350*0.10)
+            resultado=3350000-(3350000*0.10)
         }else{
             //Si se trata de un Ford Ka, el descuento es de un 15%
-            resultado=3960-(3960*0.15)
+            resultado=2900000-(2900000*0.15)
         }
     }
     document.getElementById("h_resultado").textContent ='el total es: '+ resultado
